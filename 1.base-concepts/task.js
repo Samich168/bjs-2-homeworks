9f51f2ed98a0365e5486eca1017f4736b2ee6d9f
Samich168 <abcdefg2004z@gmail.com>
2023-08-27 12:11:20 +0600

    do work

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let disc = b ** 2 - 4 * a * c;
+  if (disc > 0){
+    let x1 = ((-b + Math.sqrt(disc)) / (2 * a));
+    let x2 = ((-b - Math.sqrt(disc)) / (2 * a));
+    arr.push(x1, x2)
+  } else if (disc = 0){
+    let x1 = -b/(2*a);
+  }
   return arr;
 }
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+// function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
-}
+// }
