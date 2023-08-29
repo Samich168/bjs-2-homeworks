136fade423938a4a2c604cc88486b9fcdd610b2e
Samich168 <abcdefg2004z@gmail.com>
2023-08-29 15:20:57 +0600

    first try

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,35 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length === arr2.length){
+    if (arr1.every((el, i) => el === arr2[i])){
+        return true
+    }
+  }
+  return false
 }
 
+const people = [
+  {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
+  {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
+  {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
+  {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
+  {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
+  {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
+  {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
+  {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
+  {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
+  {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
+  {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
+  {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
+  {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
+  {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+]
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let countGender = users.filter((el) => el.gender === gender);
+  if (users.some((item) => item.gender === gender) && users.length > 0){
+    let result = countGender.reduce((acc, current) => acc + current.age, 0) / countGender.length;
+    return result;
+  }
+  return 0
 }
+console.log(getUsersNamesInAgeRange(people, 'мужской'))
