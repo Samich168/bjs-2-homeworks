5fb345cfeed423f91641e6aad983672d932f4d50
Samich168 <abcdefg2004z@gmail.com>
2023-08-31 15:47:14 +0600

    really fin task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,35 +1,34 @@
 "use strict";
 
 function Student(name, gender, age) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-    this.marks = []
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-   if (!this.marks) {
-      this.marks = [];
-    }
-    this.marks.push(...marks);
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (!this.excluded) {
+    this.marks.push(...marksToAdd);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  if (!this.marks || this.marks.length === 0) {
-      return 0;
-    }
-    const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
-    return sum / this.marks.length;
-}
+  if (this.marks.length === 0 || this.excluded) {
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  return sum / this.marks.length;
+};
 
 Student.prototype.exclude = function (reason) {
-  delete Student.marks;
-  delete Student.subject;
-  this.exclude = reason; 
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason; 
 }
 
 let student1 = new Student("Василиса", "женский", 19);
