9c5ce31567b943a0f1f4ab749b6d9bad7fb9b30f
Samich168 <abcdefg2004z@gmail.com>
2023-09-03 10:05:41 +0600

    first task

---
+++ 5.classes/task.js
@@ -0,0 +1,62 @@
+class PrintEditionItem{
+
+    constructor(name, releaseDate, pagesCount){
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix(){
+        this.state = this.state * 1.5;
+    }
+    set state(newState) {
+    if (newState < 0) {
+      this._state = 0;
+    } else if (newState > 100) {
+      this._state = 100;
+    } else {
+      this._state = newState;
+    }
+  }
+    get state(){
+        return this._state
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount){
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem{
+    constructor(author, name, releaseDate, pagesCount){
+        super(name, releaseDate, pagesCount);
+        this.type = "book"
+        this.author = author;
+    }
+}
+
+class NovelBook extends Book{
+    constructor(name, releaseDate, pagesCount, author){
+        super(name, releaseDate, pagesCount, author);
+        this.type = 'novel'
+    }
+}
+
+class DetectiveBook extends Book{
+    constructor(name, releaseDate, pagesCount, author){
+        super(name, releaseDate, pagesCount, author);
+        this.type = 'detective'
+    }
+}
+
+class FantasticBook extends Book{
+    constructor(name, releaseDate, pagesCount, author){
+        super(name, releaseDate, pagesCount, author);
+        this.type = 'fantastic'
+    }
+}
