9b6e313edef96581b83e073ac301ec1b86f82ff4
Samich168 <abcdefg2004z@gmail.com>
2023-09-05 17:14:41 +0600

    first try

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,23 @@
-﻿
+﻿function parseCount(number){
+    let result = Number.parseFloat(number);
+    try {
+        if (isNaN(result)){
+            throw new Error("Невалидное значение");
+        } 
+    }
+    catch (error){
+        console.log(error);
+    }
+    finally{
+        return result
+    }
+}
+function validateCount(count){
+    try{
+        return parseCount(count)
+    }
+    catch(error){
+        return error;
+    }
+}
+parseCount("dgdf5hdfgh")
