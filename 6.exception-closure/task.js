e7f32df2992d624766abfc17f866f4fcf3dacad4
Samich168 <abcdefg2004z@gmail.com>
2023-09-05 17:53:13 +0600

    second try

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -20,4 +20,39 @@ function validateCount(count){
         return error;
     }
 }
-parseCount("dgdf5hdfgh")
+
+class Triangle{
+    constructor(a, b, c){
+        if (a + b >= c || a + c >+ b || b + c >= a){
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+
+
+        this.a = a;
+        this.a = b;
+        this.a = c;
+    }
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+    get area(){
+        halfmeter = 0.5 * (this.perimeter);
+        area = Math.sqrt(this.halfmeter * (this.halfmeter - this.a)*(this.halfmeter - this.b)*(this.halfmeter - this.c))
+        return parseFloat(area.toFixed(3))
+    }  
+    
+}
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+    };
+  }
+}
