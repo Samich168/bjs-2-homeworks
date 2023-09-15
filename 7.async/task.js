1af353dd8faeaa09b845ca1dda6925b810f6fecc
Samich168 <abcdefg2004z@gmail.com>
2023-09-15 10:49:59 +0600

    last?

---
+++ 7.async/task.js
@@ -0,0 +1,55 @@
+class AlarmClock {
+    constructor(){
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+    addClock(time, callback) {
+        if(!time || !callback){
+            throw new Error("Отсутствуют обязательные аргументы")
+        }
+        if(this.alarmCollection.some(alarm => alarm.time === time)) {
+            console.warn("Уже присутствует звонок на это время");
+        }
+        this.alarmCollection.push({callback: callback, time: time, canCall: true});
+    }
+    removeClock(time){
+        this.alarmCollection = this.alarmCollection.filter(t => t.time != time);
+    }
+    getCurrentFormattedTime(){
+        const now = Date()
+        const currentHour = now.getHours().padStart(2, "0");
+        const currentMinute = now.getMinutes().padStart(2, "0");
+
+        return (`Текущее время: ${currentHour}:${currentMinute}`)
+    }
+    start(){
+        if(!this.intervalId){
+            this.intervalId = setInterval(() => {
+                const currentTime = this.getCurrentFormattedTime();
+
+                this.alarmCollection.forEach((alarm) =>{
+                    if(alarm.time === currentTime && alarm.canCall){
+                        canCall = false;
+                        alarm.callback();
+                    }
+                })
+            }, 1000)
+        }   
+    }
+    stop(){
+        if(this.intervalId){
+            clearInterval()
+            this.intervalId = null;
+        }
+        
+    }
+    resetAllCalls(){
+        this.alarmCollection.forEach((call) => {
+            call.canCall = true;
+        })
+    }
+    clearAlarms(){
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
