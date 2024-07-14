50a4e79e0eefe28a489392cfbe5f5c98c0304142
Samich168 <abcdefg2004z@gmail.com>
2024-07-14 10:54:21 +0600

    1.1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -6,29 +6,26 @@ function Student(name, gender, age) {
   this.age = age;
   this.marks = [];
 }
-
-Student.prototype.setSubject = function (subjectName) {
+Student.prototype.setSubject = function(subjectName) {
   this.subject = subjectName;
 }
-
-Student.prototype.addMarks = function (...marksToAdd) {
-  if (!this.excluded) {
-    this.marks.push(...marksToAdd);
-  }
-};
-
-Student.prototype.getAverage = function () {
-  if (this.marks.length === 0 || this.excluded) {
+Student.prototype.addMarks = function(...marksToAdd){
+  if (!this.marks){
+    return 0;
+  };
+  this.marks.push(...marksToAdd);
+}
+Student.prototype.getAverage = function(){
+  if (!this.marks || this.marks.length === 0){
     return 0;
   }
   const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
   return sum / this.marks.length;
-};
-
-Student.prototype.exclude = function (reason) {
-  delete this.subject;
+}
+Student.prototype.exclude = function(reason){
   delete this.marks;
-  this.excluded = reason; 
+  delete this.subject;
+  this.excluded = reason;
 }
 
 let student1 = new Student("Василиса", "женский", 19);
@@ -42,4 +39,4 @@ let student2 = new Student("Артём", "мужской", 25);
 student2.setSubject("Geometry");
 student2.exclude('плохая учёба')
 console.log(student2)
-
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
